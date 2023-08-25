58e038b4afccc92b738d20df177f95b87555411f
Kevin Damm <github@kevindamm.com>
2023-08-25 19:17:15 -0400

    base game metadata definition

---
+++ gamewiki/game-info.go
@@ -0,0 +1,89 @@
+package gamewiki
+
+type GamesList struct {
+	Games []GameInfo
+}
+
+// Represents metadata and derived data about a single game.  Includes solo
+// and multiplayer games, deterministic and nondeterministic, turn-based and
+// real-time, table
+type GameInfo struct {
+	// Identifies the game; limited URL-safe characters.
+	GameId string `json:"id"`
+
+	// The displayed name of the game.
+	Name string `json:"name"`
+
+	// A string representation of the number of players allowed by the game rules.
+	RoleCount string `json:"players"`
+
+	// A brief description of the game (longer descriptions found in STORY.md).
+	Summary string `json:"summary"`
+
+	// Reflects the playability of the game and its associated rulesheet & UI.
+	Playable GameStatus `json:"status"`
+
+	// A list of tags/labels assigned to this game.
+	Tags []string `json:"tags"`
+
+	// The description part of an external reference link.
+	RefDesc string `json:"ref_desc"`
+
+	// The URL pointed to by the external reference link.
+	RefLink string `json:"ref_link"`
+
+	// Local path where the game's rule definition can be found (if it exists).
+	Rulesheet string `json:"rulesheet,omitempty"`
+	// Additional notes I was making when first compiling this list.
+	ExtraNotes []string `json:"notes,omitempty"`
+
+	// The measurable features of this game.
+	Measures GameMeasures `json:"measures,omitempty"`
+
+	// Comparative values for this game relative to other games.
+	Scores GameScores `json:"scores,omitempty"`
+}
+
+type GameStatus string
+
+// These definitions are self-documenting.
+const (
+	UNKNOWN      GameStatus = ""
+	DOCUMENTED   GameStatus = "documented, but not implemented"
+	IN_DEVELOP   GameStatus = "currently in development"
+	ALPHA_DEV    GameStatus = "ALPHA (development + testing)"
+	BETA_TEST    GameStatus = "BETA (feature complete, in beta testing)"
+	PRIVATE_ONLY GameStatus = "HIDDEN"
+	LAUNCHED     GameStatus = "LAUNCHED!"
+	ARCHIVED     GameStatus = "[archived]"
+)
+
+// Game Complexity is a game design measurement, derived from game properties
+// (e.g. how deep & dense the game tree is, how much of the board is used in a
+// game, percentage of pieces in play, etc.).  These measures can sometimes be
+// measured directly from the rules but many of these measures require a
+// simulation of many playthroughs to sufficiently approximate an estimate.
+type GameComplexity struct {
+	BoardUsage           float32 `json:"Board Usage"`
+	MinMatchLength       uint    `json:"min(Match Length)"`
+	MaxMatchLength       uint		 `json:"max(Match Length)"`
+	RuleComplexity       float32 `json:"Rule Complexity"`
+	EquipmentComplexity  float32 `json:"Equipment Complexity"`
+	BranchingFactor      float32 `json:"Branching Factor"`
+	GameTreeComplexity   float32 `json:"Game Tree Complexity"`
+	GameLength           float32 `json:"Game Length"`
+	StateSpaceComplexity float32 `json:"Statespace Complexity"`
+	MoveSeparability     float32 `json:"Move Separability"`
+	MutationalComplexity float32 `json:"Mutational Complexity"`
+}
+
+// Measurements based on the complexity scores above, attempting to model the
+// qualities of a game relative to other (similar and different) games.
+type GameMeasures struct {
+	Simplicity    float32 `json:"Simplicity"`
+	Clarity       float32 `json:"Clarity"`
+	Efficiency    float32 `json:"Efficiency"`
+	Depth         float32 `json:"Relative Depth"`
+	Elegance      float32 `json:"Elegance"`
+	Shibui        float32 `json:"Shibui"`
+}
