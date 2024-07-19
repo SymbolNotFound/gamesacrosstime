42e40197bfbcd85060bd394fdb9b66cc3bc78308
Kevin Damm <github@kevindamm.com>
2024-07-18 20:22:04 -0400

    filter for games which appear in multiple collections/sources

--- combiner.go
+++ combiner.go
@@ -8,6 +8,9 @@ import (
 )
 
 func main() {
+	exists := make(map[string]bool)
+	multix := make(map[string]bool)
+
 	file, err := os.Open("bgg.jsonl")
 	if err != nil {
 		fmt.Println(err)
@@ -16,17 +19,79 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	count := 0
 	for scanner.Scan() {
 		var info map[string]any
 		err := json.Unmarshal([]byte(scanner.Text()), &info)
 		if err != nil {
-			fmt.Printf("ERROR: %s\n", err)
-			break
+			fmt.Printf("(1) ERROR: %s\n", err)
+			file.Close()
+			return
 		}
-		fmt.Printf("%v\n", info)
-		count += 1
+		id, found := info["id"]
+		if !found {
+			continue
+		}
+		exists[id.(string)] = true
 	}
+	file.Close()
 
-	fmt.Printf("\nparsed %d games\n", count)
+	file, err = os.Open("asg-all.jsonl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	scanner = bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		var info map[string]any
+		err := json.Unmarshal([]byte(scanner.Text()), &info)
+		if err != nil {
+			fmt.Printf("(2) ERROR: %s\n", err)
+			file.Close()
+			return
+		}
+		id, found := info["id"]
+		if !found {
+			continue
+		}
+		if exists[id.(string)] {
+			multix[id.(string)] = true
+		} else {
+			exists[id.(string)] = true
+		}
+	}
+	file.Close()
+
+	file, err = os.Open("wikia.jsonl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	scanner = bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		var info map[string]any
+		text := []byte(scanner.Text())
+		err := json.Unmarshal(text, &info)
+		if err != nil {
+			fmt.Printf("(3) ERROR: %s\n", err)
+			fmt.Println(string(text))
+			file.Close()
+			return
+		}
+		id, found := info["id"]
+		if !found {
+			continue
+		}
+		if exists[id.(string)] {
+			multix[id.(string)] = true
+		} else {
+			exists[id.(string)] = true
+		}
+	}
+	file.Close()
+
+	fmt.Printf("found %d unique ids and %d appearing in more than one collection", len(exists), len(multix))
 }
